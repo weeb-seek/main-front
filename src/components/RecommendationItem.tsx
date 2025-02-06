55f16d5406bfdd29a19f337237f07db491da0445
Vladislav <rushella.ln@outlook.com>
2025-02-06 19:44:29 +0500

    introduced RecommendationItem component

---
+++ src/components/RecommendationItem.tsx
@@ -0,0 +1,16 @@
+type RecommendationItem = {
+    imageSrc: string,
+    title: string,
+    description: string,
+    score: number
+}
+
+function RecommendationItem() {
+    return (
+        <>
+            ...
+        </>
+    )
+}
+
+export default RecommendationItem
