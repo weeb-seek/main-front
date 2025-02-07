6286d4e28ceddc61a6214522b2fd4f56688f3e01
Vladislav <rushella.ln@outlook.com>
2025-02-07 18:45:15 +0500

    added some css for RecommendationItem component

--- src/components/RecommendationItem.tsx
+++ src/components/RecommendationItem.tsx
@@ -8,13 +8,13 @@ type RecommendationItemProps = {
 function RecommendationItem({ title, description, imageSrc, score }: RecommendationItemProps) {
     return (
         <>
-            <div>
-                <span>{title}</span>
+            <div className="recommendation-item">
+                <div className="gradient"/>
                 <img src={imageSrc} alt={title}/>
-                <span>Score: {score}</span>
-            </div>
-            <div>
-                {description}
+                <div className="info">
+                    <span className="info-title">{title}</span>
+                    <span className="info-score"><span className="info-score">{score}</span></span>
+                </div>
             </div>
         </>
     )
