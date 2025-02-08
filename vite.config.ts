50cd86fecf326b93d7b8cec7bcecd99bdbf04782
Vladislav <rushella.ln@outlook.com>
2025-02-09 00:48:03 +0500

    changed configs for deployment in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
+    base: "/main-front/",
+    plugins: [
       react()
   ],
 })
